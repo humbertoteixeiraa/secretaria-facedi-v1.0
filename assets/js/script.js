7b503e6fd2df30056f472b0af46c9e0bc630a36d
Humberto Teixeira <humberto.quim@gmail.com>
2022-05-04 16:37:42 -0300

    Alerta criado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,3 @@
-function clicou1() {
-    alert("Você clicou no botão!")
-}
-
-function clicou2() {
-    document.getElementById("title-text").innerHTML = "Changed Text";
-}
-
-function passou() {
-    alert("Você passou o mouse pelo quadrado!")
-}
-
-function retirou() {
-    alert("Você retirou o mouse do quadrado!")
+function alerta() {
+    alert("Não há declarações de defesa! \n\nSemestre trancado institucionalmente.")
 }
