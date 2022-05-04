9afff8c0d5b237ff9b29145c32963c1fbb9700dc
Humberto Teixeira <humberto.quim@gmail.com>
2022-05-04 16:17:02 -0300

    Início da criação do script de alerta

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function clicou1() {
+    alert("Você clicou no botão!")
+}
+
+function clicou2() {
+    document.getElementById("title-text").innerHTML = "Changed Text";
+}
+
+function passou() {
+    alert("Você passou o mouse pelo quadrado!")
+}
+
+function retirou() {
+    alert("Você retirou o mouse do quadrado!")
+}
